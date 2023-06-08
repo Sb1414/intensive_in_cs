35b6c30b14de60bf3f87091870e541acd02a0c99
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-08 16:24:34 +0300

    ex05

---
+++ d01/d01_ex05/d01_ex00/CustomerExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d01_ex00
+{
+    internal static class CustomerExtensions
+    {
+        public static CashRegister ChooseCashRegisterByFewestCustomers(this Customer customer, IEnumerable<CashRegister> cashRegisters)
+        {
+            // с наименьшим количеством покупателей
+            return cashRegisters.OrderBy(cr => cr.GetCustomersCount()).FirstOrDefault();
+        }
+
+        public static CashRegister ChooseCashRegisterByFewestItems(this Customer customer, IEnumerable<CashRegister> cashRegisters)
+        {
+            // с наименьшим количеством товаров среди всех покупателей в очереди
+            return cashRegisters.OrderBy(cr => cr.GetTotalItemsInQueue()).FirstOrDefault();
+        }
+    }
+
+}
