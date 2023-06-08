0dd0c99a6b4203359da0973f7f84a6a8f763286f
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-08 15:47:26 +0300

    ex03

---
+++ d01/d01_ex03/d01_ex00/d01_ex00/CashRegister.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d01_ex00
+{
+    internal class CashRegister
+    {
+        private readonly string name;
+        private Queue<Customer> customers;
+
+        public string Name { get { return name; } }
+
+        public CashRegister(string name)
+        {
+            this.name = name;
+            customers = new Queue<Customer>();
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            customers.Enqueue(customer);
+        }
+
+        public Customer ServeNextCustomer()
+        {
+            return customers.Dequeue();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public static bool operator ==(CashRegister register1, CashRegister register2)
+        {
+            if (ReferenceEquals(register1, register2))
+                return true;
+
+            if (register1 is null || register2 is null)
+                return false;
+
+            return register1.Name == register2.Name;
+        }
+
+        public static bool operator !=(CashRegister register1, CashRegister register2)
+        {
+            return !(register1 == register2);
+        }
+
+    }
+}
