4e5d1805c9cfc19200b5dd05888c89645703ae06
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-08 16:13:56 +0300

    ex04

---
+++ d01/d01_ex04/d01_ex00/Program.cs
@@ -0,0 +1,67 @@
+﻿// See https://aka.ms/new-console-template for more information
+using d01_ex00;
+using System;
+
+// ex00
+Console.WriteLine("\n============ ex00 ============\n");
+Storage storage = new Storage(100);
+
+if (storage.IsEmpty())
+{
+    Console.WriteLine("Storage is empty.");
+}
+else
+{
+    Console.WriteLine("Storage is not empty.");
+}
+
+// ex01
+Console.WriteLine("\n============ ex01 ============\n");
+Customer customer = new Customer(1, "Andrew");
+Console.WriteLine(customer.ToString());
+
+var customer1 = new Customer(1, "Andrew");
+var customer2 = new Customer(1, "Andrew");
+
+Console.WriteLine(customer1 == customer2);
+
+// ex02
+Console.WriteLine("\n============ ex02 ============\n");
+var customer3 = new Customer(1, "Andrew");
+var customer4 = new Customer(2, "John");
+var customer5 = new Customer(3, "Emily");
+
+customer3.FillCart(15);
+customer4.FillCart(15);
+customer5.FillCart(15);
+
+Console.WriteLine(customer3);
+Console.WriteLine(customer4);
+Console.WriteLine(customer5);
+
+// ex03
+Console.WriteLine("\n============ ex03 ============\n");
+var cashRegister1 = new CashRegister("Register #1");
+var cashRegister2 = new CashRegister("Register #1");
+
+Console.WriteLine(cashRegister1 == cashRegister2);
+
+var customer6 = new Customer(1, "Andrew");
+
+cashRegister1.AddCustomer(customer6);
+customer6.FillCart(15);
+
+Console.WriteLine(cashRegister1);
+
+var servedCustomer = cashRegister1.ServeNextCustomer();
+Console.WriteLine(servedCustomer);
+
+// ex04
+Console.WriteLine("\n============ ex04 ============\n");
+
+var store = new Store(100, 3);
+
+Console.WriteLine($"Размер: {store.StorageCapacity}");
+Console.WriteLine($"Кассовые аппараты: {string.Join(", ", store.CashRegisters)}");
+Console.WriteLine(store.IsOpen());  // Output: Is Open? True
+
---
+++ d01/d01_ex04/d01_ex00/Store.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d01_ex00
+{
+    internal class Store
+    {
+        private int storageCapacity;
+        private List<string> cashRegisters;
+
+        public int StorageCapacity { get { return storageCapacity; } }
+        public IReadOnlyList<string> CashRegisters { get { return cashRegisters.AsReadOnly(); } }
+
+        public Store(int capacity, int countCashReg)
+        {
+            this.storageCapacity = capacity;
+
+            cashRegisters = new List<string>();
+            for (int i = 1; i <= countCashReg; i++)
+            {
+                cashRegisters.Add($"Cash Register #{i}");
+            }
+        }
+
+        public bool IsOpen() // возвращает true, если на складе еще есть товар
+        {
+            return storageCapacity > 0;
+        }
+    }
+}
