dc9c6e51bed591cf14286c81744605c57108e7ba
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-08 14:16:15 +0300

    ex01

---
+++ d01/d01_ex01/d01_ex00/d01_ex00/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d01_ex00
+{
+    internal class Customer
+    {
+        private readonly int customerNumber;
+        private readonly string name;
+
+        public int CustomerNumber { get { return customerNumber; } }
+        public string Name { get { return name; } }
+
+        public Customer(int customerNumber, string name)
+        {
+            this.customerNumber = customerNumber;
+            this.name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Customer other)
+                return false;
+
+            return customerNumber == other.customerNumber && name == other.name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, customer #{CustomerNumber}";
+        }
+
+        public static bool operator ==(Customer customer1, Customer customer2)
+        {
+            if (ReferenceEquals(customer1, customer2))
+                return true;
+
+            if (customer1 is null || customer2 is null)
+                return false;
+
+            return customer1.Equals(customer2);
+        }
+
+        public static bool operator !=(Customer customer1, Customer customer2)
+        {
+            return !(customer1 == customer2);
+        }
+    }
+}
---
+++ d01/d01_ex01/d01_ex00/d01_ex00/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using d01_ex00;
+
+Storage storage = new Storage(100);
+
+if (storage.IsEmpty())
+{
+    Console.WriteLine("Storage is empty.");
+}
+else
+{
+    Console.WriteLine("Storage is not empty.");
+}
+
+Customer customer = new Customer(1, "Andrew");
+Console.WriteLine(customer.ToString());
+
+var customer1 = new Customer(1, "Andrew");
+var customer2 = new Customer(1, "Andrew");
+
+Console.WriteLine(customer1 == customer2);
