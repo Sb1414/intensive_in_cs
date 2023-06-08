71442f3f4410b39c7bcaa0cddb9238ec9d9e0dbb
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-08 14:04:59 +0300

    Storage d01_ex00

---
+++ d01/d01_ex00/d01_ex00/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using d01_ex00;
+
+Storage storage = new Storage(100);
+
+if (storage.IsEmpty())
+{
+    Console.WriteLine("Storage is empty.");
+}
+else
+{
+    Console.WriteLine("Storage is not empty.");
+}
---
+++ d01/d01_ex00/d01_ex00/Storage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d01_ex00
+{
+    internal class Storage
+    {
+        private int capacity; // емкость
+        private int currentQuantity; // начальная стоимость?
+
+        public Storage(int capacity)
+        {
+            Capacity = capacity;
+            CurrentQuantity = capacity;
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public int CurrentQuantity
+        {
+            get { return currentQuantity; }
+            set { currentQuantity = value; }
+        }        
+
+        public bool IsEmpty() => CurrentQuantity == 0;
+    }
+}
