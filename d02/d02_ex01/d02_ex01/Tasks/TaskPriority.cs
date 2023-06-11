5a65c64a97c6def86662f6fd1aff483705c333fa
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-11 13:25:56 +0300

    enums in d02_ex01

---
+++ d02/d02_ex01/d02_ex01/Tasks/TaskPriority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d02_ex01.Tasks
+{
+    enum TaskPriority
+    {
+        Low,
+        Normal,
+        High
+    }
+}
---
+++ d02/d02_ex01/d02_ex01/Tasks/TaskType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace d02_ex01.Tasks
+{
+    enum TaskType
+    {
+        Work,
+        Study,
+        Personal
+    }
+}
