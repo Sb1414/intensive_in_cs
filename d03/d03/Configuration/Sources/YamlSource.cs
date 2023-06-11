7828d3203389d662a265cc9d9e902af94af0b5ed
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-11 18:00:30 +0300

    yml not fix

--- d03/d03/Configuration/Sources/YamlSource.cs
+++ d03/d03/Configuration/Sources/YamlSource.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace d03.Sources
 {
-    internal class YamlSource
+    internal class YamlSource : IConfigurationSource
     {
+        private string filePath;
+
+        public YamlSource(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public Dictionary<string, object> LoadParameters()
+        {
+            var deserializer = new DeserializerBuilder()
+                .WithNamingConvention(CamelCaseNamingConvention.Instance)
+                .Build();
+
+            string yamlContent = File.ReadAllText(filePath);
+            var parameters = deserializer.Deserialize<Dictionary<string, object>>(yamlContent);
+            return parameters;
+        }
     }
 }
