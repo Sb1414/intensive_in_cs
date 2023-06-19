0134610a81ea5e007aab3f45da12c7374494d625
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-19 20:50:37 +0300

    ViewModels

--- team00/rush00/ViewModels/MainWindowViewModel.cs
+++ team00/rush00/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,89 @@
-﻿namespace rush00.ViewModels;
+﻿using System;
+using Prism.Commands;
+using System.Windows.Input;
+using rush00.Models;
+using rush00.Views;
+
+namespace rush00.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string habitTitle;
+        private string motivation;
+        private int numberOfDays;
+        private DateTime startDate;
+
+        public string HabitTitle
+        {
+            get { return habitTitle; }
+            set
+            {
+                if (habitTitle != value)
+                {
+                    habitTitle = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Motivation
+        {
+            get { return motivation; }
+            set
+            {
+                if (motivation != value)
+                {
+                    motivation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public int NumberOfDays
+        {
+            get { return numberOfDays; }
+            set
+            {
+                if (numberOfDays != value)
+                {
+                    numberOfDays = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set
+            {
+                if (startDate != value)
+                {
+                    startDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand StartCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            StartCommand = new DelegateCommand(StartTracking);
+        }
+
+        private void StartTracking()
+        {
+            // Проверка и обработка введенных данных
+            if (string.IsNullOrEmpty(HabitTitle) || string.IsNullOrEmpty(Motivation) || NumberOfDays <= 0)
+            {
+                return;
+            }
+
+            var habit = new Habit(HabitTitle, Motivation, StartDate, NumberOfDays);
+            // Необходимо сохранить или передать habit в другую часть приложения для дальнейшего использования
+
+            var trackingView = new TrackingScreen(habit);
+            trackingView.Show();
+        }
 }
--- team00/rush00/ViewModels/ViewModelBase.cs
+++ team00/rush00/ViewModels/ViewModelBase.cs
@@ -1,8 +1,15 @@
-﻿using ReactiveUI;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ReactiveUI;
 
 namespace rush00.ViewModels;
 
 public class ViewModelBase : ReactiveObject
 {
-    
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    } 
 }
