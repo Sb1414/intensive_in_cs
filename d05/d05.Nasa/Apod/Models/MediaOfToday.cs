beedcfb25e01942ec549568ea038df7483904a6b
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-21 16:38:15 +0300

    Model

--- d05/d05.Nasa/Apod/Models/MediaOfToday.cs
+++ d05/d05.Nasa/Apod/Models/MediaOfToday.cs
@@ -1,6 +1,27 @@
+using System.Globalization;
+using System.Text.Json.Serialization;
+
 namespace d05.Nasa.Apod.Models;
 
 public class MediaOfToday
 {
+    [JsonPropertyName("copyright")] public string Copyright { get; set; }
+    [JsonPropertyName("date")] public DateTime Date { get; set; }
+    [JsonPropertyName("explanation")] public string Explanation { get; set; }
+    [JsonPropertyName("title")] public string Title { get; set; }
+    [JsonPropertyName("url")] public string Url { get; set; }
     
+    public void ReplaceNewLinesInCopyright()
+    {
+        Copyright = Copyright?.Replace("\n", " ");
+        Copyright = Copyright?.Trim(' ');
+    }
+
+    public override string ToString()
+    {
+        ReplaceNewLinesInCopyright();
+        return $"{Date.ToString("dd'/'MM'/'yyyy", new CultureInfo("en-GB"))}\n" +
+            $"‘{Title}’ " + $"{(string.IsNullOrEmpty(Copyright) ? "" : $"by {Copyright}")}\n" +
+            $"{Explanation}\n{Url}\n";
+    }
 }
