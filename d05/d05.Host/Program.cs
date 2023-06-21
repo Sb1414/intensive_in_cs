1e86a26d5eb708b12887ed22f6d11480a3374e7f
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-21 16:38:32 +0300

    host

--- d05/d05.Host/Program.cs
+++ d05/d05.Host/Program.cs
@@ -1,3 +1,61 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using d05.Nasa;
+using d05.Nasa.Apod;
+using d05.Nasa.Apod.Models;
+using Microsoft.Extensions.Configuration;
+
+// if (args.Length < 2)
+// {
+//     Console.WriteLine("Usage: dotnet run <apiKey> <count>");
+//     return;
+// }
+//
+// string apiKey = args[0];
+// int count = int.Parse(args[1]);
+//
+// var apodClient = new ApodClient(apiKey);
+// var mediaOfTodayArray = await apodClient.GetAsync(count);
+//
+// if (mediaOfTodayArray != null)
+// {
+//     foreach (var mediaOfToday in mediaOfTodayArray)
+//     {
+//         Console.WriteLine($"{mediaOfToday.Date}\n'{mediaOfToday.Title}' by {mediaOfToday.Copyright}");
+//         Console.WriteLine(mediaOfToday.Explanation);
+//         Console.WriteLine(mediaOfToday.Url);
+//         Console.WriteLine();
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Failed to retrieve data from NASA API.");
+// }
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: dotnet run <apiKey> <count>");
+    return;
+}
+
+string apiKey = args[0];
+int count = int.Parse(args[1]);
+
+var apodClient = new ApodClient(apiKey);
+var mediaOfTodayArray = await apodClient.GetAsync(count);
+
+if (mediaOfTodayArray != null)
+{
+    foreach (var mediaOfToday in mediaOfTodayArray)
+    {
+        // Console.WriteLine($"{mediaOfToday.Date}\n'{mediaOfToday.Title}' by {mediaOfToday.Copyright}");
+        // Console.WriteLine(mediaOfToday.Explanation);
+        // Console.WriteLine(mediaOfToday.Url);
+        // Console.WriteLine();
+        Console.WriteLine(mediaOfToday.ToString());
+    }
+}
+else
+{
+    Console.WriteLine("Failed to retrieve data from NASA API.");
+}
