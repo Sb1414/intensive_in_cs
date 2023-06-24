dc4a0aa1c4573ddcb7c3a613dc7743460abcdbc5
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-25 00:24:51 +0300

    d07 ex03 Models

---
+++ d07/d07_ex03/Models/IdentityRole.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+
+namespace d07_ex03.Models;
+
+public class IdentityRole
+{
+    public IdentityRole()
+    {
+    }
+
+    [Description("Name")]
+    [DefaultValue("Moderator")] public string Name { get; set; }
+    [Description("Description")]
+    [DefaultValue("A role for moderation")] public string Description { get; set; }
+
+    public override string ToString()
+        => $"{Name}, {Description}";
+}
