e070b9e19841f3ff23d0dfb92891d9d4d1b3a311
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-06-25 00:10:11 +0300

    ex01 d07

---
+++ d07/d07_ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+
+DefaultHttpContext context = new DefaultHttpContext();
+
+Console.WriteLine("Old Response value: " + context.Response);
+
+Type contextType = typeof(DefaultHttpContext);
+
+FieldInfo responseField = contextType.GetField("_response", BindingFlags.Instance | BindingFlags.NonPublic);
+
+responseField.SetValue(context, null);
+
+Console.WriteLine("New Response value: " + context.Response);
