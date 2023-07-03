21a04e12dcbce970961ce7051abe374c79cf18d7
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-03 11:39:32 +0300

    d00 ex00 add enGBCulture

--- d00/d00_ex00/Program.cs
+++ d00/d00_ex00/Program.cs
@@ -16,6 +16,7 @@ try
     {
         throw new Exception("Something went wrong. Check your input and retry.");
     }
+    CultureInfo enGBCulture = new CultureInfo("en-GB");
 
     double monthlyInterestRate = annualPercentageRate / 12 / 100;
 
@@ -24,7 +25,8 @@ try
 
     double totalDebtBalance = loanAmount;
 
-    DateTime currentDate = DateTime.Now;
+    // DateTime currentDate = DateTime.Now;
+    DateTime currentDate = new DateTime(2021, 5, 1);
     for (int paymentNumber = 1; paymentNumber <= numberOfMonths; paymentNumber++)
     {
         DateTime paymentDate = currentDate.AddMonths(paymentNumber);
@@ -33,8 +35,8 @@ try
         totalDebtBalance -= principalPayment;
         double roundedTotalDebtBalance = Math.Round(totalDebtBalance, 2); // чтобы не было -0 в конце
 
-        Console.WriteLine($"{paymentNumber,-9}\t{paymentDate.ToString("MM/dd/yyyy"),-12}\t{monthlyPayment.ToString("N2"),13}\t" +
-        $"{principalPayment.ToString("N2"),12}\t{interestPayment.ToString("N2"),15}\t{roundedTotalDebtBalance.ToString("N2"),15}");
+        Console.WriteLine($"{paymentNumber,-9}\t{paymentDate.ToString("d", enGBCulture),-12}\t{monthlyPayment.ToString("N2", enGBCulture),13}\t" +
+                $"{principalPayment.ToString("N2", enGBCulture),12}\t{interestPayment.ToString("N2", enGBCulture),15}\t{roundedTotalDebtBalance.ToString("N2", enGBCulture),15}");
 
     }
 
