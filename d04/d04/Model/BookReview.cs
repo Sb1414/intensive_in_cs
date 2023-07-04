51570863a30ab84397d1380b528f2d036996093d
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-04 15:35:27 +0300

    d04 ex00

---
+++ d04/d04/Model/BookReview.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace d04.Model
+{
+
+    public class BookReview
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("rank")]
+        public int Rank { get; set; }
+
+        [JsonPropertyName("list_name")]
+        public string ListName { get; set; }
+
+        [JsonPropertyName("summary_short")]
+        public string SummaryShort { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} by {Author} [{Rank} on NYT’s {ListName}]\n{SummaryShort}\n{Url}";
+        }
+    }
+
+    public class RootObject
+    {
+        [JsonPropertyName("results")]
+        public BookResult[] Results { get; set; }
+    }
+
+    public class BookResult
+    {
+        [JsonPropertyName("list_name")]
+        public string ListName { get; set; }
+
+        [JsonPropertyName("book_details")]
+        public BookDetail[] BookDetails { get; set; }
+
+        [JsonPropertyName("amazon_product_url")]
+        public string AmazonProductUrl { get; set; }
+
+        [JsonPropertyName("rank")]
+        public int Rank { get; set; }
+    }
+
+    public class BookDetail
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("author")]
+        public string Author { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+    }
+
+}
---
+++ d04/d04/Program.cs
@@ -0,0 +1,40 @@
+﻿using d04.Model;
+using Microsoft.Extensions.Configuration.Json;
+using System.Reflection;
+using System.Text.Json;
+
+string currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+string currentDirectory2 = currentDirectory.Substring(0, currentDirectory.Length - 16);
+string filePath = currentDirectory2 + "book_reviews.json"; // для винды
+// string filePath = "book_reviews.json"; // для мака
+
+string json = File.ReadAllText(filePath);
+
+// Десериализация JSON в объекты
+RootObject data = JsonSerializer.Deserialize<RootObject>(json);
+
+// Создание списка объектов BookReview из данных JSON
+BookReview[] bookReviews = new BookReview[data.Results.Length];
+for (int i = 0; i < data.Results.Length; i++)
+{
+    BookResult result = data.Results[i];
+    BookDetail bookDetail = result.BookDetails[0];
+
+    BookReview bookReview = new BookReview
+    {
+        Title = bookDetail.Title,
+        Author = bookDetail.Author,
+        Rank = result.Rank,
+        ListName = result.ListName,
+        SummaryShort = bookDetail.Description,
+        Url = result.AmazonProductUrl
+    };
+
+    bookReviews[i] = bookReview;
+}
+
+foreach (BookReview bookReview in bookReviews)
+{
+    Console.WriteLine(bookReview.ToString() + "\n");
+}
