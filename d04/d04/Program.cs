48b2d24f7f8c92a3305c48c13c8b8b8179603b00
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-05 12:26:59 +0300

    main

--- d04/d04/Program.cs
+++ d04/d04/Program.cs
@@ -20,11 +20,18 @@ while (fl)
             case 0:
                 draw();
                 ex00();
+                print_ex00();
                 draw();
                 break;
             case 1:
                 draw();
                 ex01();
+                print_ex01();
+                draw();
+                break;
+            case 2:
+                draw();
+                ex02();
                 draw();
                 break;
             default:
@@ -38,6 +45,9 @@ while (fl)
     }
 }
 
+BookReview[] bookReviews;
+RootObjectMovieReview data;
+
 void ex00()
 {
     string filePath = currentDirectory2 + "book_reviews.json"; // для винды
@@ -46,13 +56,13 @@ void ex00()
     string json = File.ReadAllText(filePath);
 
     // Десериализация JSON в объекты
-    RootObjectBookReview data = JsonSerializer.Deserialize<RootObjectBookReview>(json);
+    RootObjectBookReview data_ = JsonSerializer.Deserialize<RootObjectBookReview>(json);
 
     // Создание списка объектов BookReview из данных JSON
-    BookReview[] bookReviews = new BookReview[data.Results.Length];
-    for (int i = 0; i < data.Results.Length; i++)
+    bookReviews = new BookReview[data_.Results.Length];
+    for (int i = 0; i < data_.Results.Length; i++)
     {
-        BookResult result = data.Results[i];
+        BookResult result = data_.Results[i];
         BookDetail bookDetail = result.BookDetails[0];
 
         BookReview bookReview = new BookReview
@@ -67,7 +77,10 @@ void ex00()
 
         bookReviews[i] = bookReview;
     }
+}
 
+void print_ex00()
+{
     foreach (BookReview bookReview in bookReviews)
     {
         Console.WriteLine(bookReview.ToString() + "\n");
@@ -82,14 +95,63 @@ void ex01()
     string json = File.ReadAllText(filePath);
 
     // Десериализация JSON в объекты
-    RootObjectMovieReview data = JsonSerializer.Deserialize<RootObjectMovieReview>(json);
+    data = JsonSerializer.Deserialize<RootObjectMovieReview>(json);
+}
 
+void print_ex01()
+{
     // Вывод списка фильмов в консоль
     foreach (MovieReview movie in data.Results)
     {
         Console.WriteLine(movie.ToString() + "\n");
     }
 }
+
+void ex02()
+{
+    ex00();
+    ex01();
+    Console.WriteLine("Input search text:");
+    string searchText = Console.ReadLine();
+
+    // Поиск фильмов по заголовку
+    MovieReview[] movieSearchResult = data.Results.Search(searchText);
+
+    // Поиск книг по заголовку
+    BookReview[] bookSearchResult = bookReviews.Search(searchText);
+
+    // Объединение результатов поиска книг и фильмов
+    var combinedResults = movieSearchResult.Cast<ISearchable>()
+        .Concat(bookSearchResult.Cast<ISearchable>());
+
+    // Преобразование в массив результатов
+    ISearchable[] searchResult = combinedResults.ToArray();
+
+    if (searchResult.Length == 0)
+    {
+        Console.WriteLine($"Not found");
+    }
+    else
+    {
+        Console.WriteLine($"\nItems found: {searchResult.Length}\n");
+
+        // Группировка результатов по типу (фильм/книга)
+        var groupedResults = searchResult.GroupBy(item => item.GetType().Name)
+            .Select(group => new { Type = group.Key, Results = group.OrderBy(item => item.Title) });
+
+        foreach (var group in groupedResults)
+        {
+            Console.WriteLine($"{group.Type} search result [{group.Results.Count()}]:");
+            foreach (var item in group.Results)
+            {
+                Console.WriteLine($"- {item.Title}");
+                Console.WriteLine($"{item}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
 
 void menu()
 {
