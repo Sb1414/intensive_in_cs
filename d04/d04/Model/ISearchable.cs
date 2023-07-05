4e9b46fd884630560461914c31bc03699f809df5
Sb1414 <kalkamanova.sabina14@gmail.com>
2023-07-05 12:25:21 +0300

    add interface for search

--- d04/d04/Model/ISearchable.cs
+++ d04/d04/Model/ISearchable.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace d04.Model
 {
-    internal class ISearchable
+    public interface ISearchable
     {
+        string Title { get; }
     }
+
+    public static class EnumerableExtensions
+    {
+        public static T[] Search<T>(this IEnumerable<T> list, string search) where T : ISearchable
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                return list.ToArray();
+            }
+
+            string searchLower = search.ToLower();
+
+            return list.Where(item => item.Title.ToLower().Contains(searchLower))
+                .OrderBy(item => item.Title)
+                .ToArray();
+        }
+    }
+
 }
